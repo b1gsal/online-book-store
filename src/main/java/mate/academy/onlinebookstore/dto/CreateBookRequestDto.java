43b6f591188c3bf494d7cbbb645ecd83b31328a0
Denya Basarab <denya2007basarab@gmail.com>
2026-07-25 23:54:27 +0300

    refactor: delete unnecessary annotation

--- src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,11 @@
 package mate.academy.onlinebookstore.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
     private String title;
     private String author;
