cc4e50b9b376e2e602eab91e9e6ccda5095fd911
Denya Basarab <denya2007basarab@gmail.com>
2026-07-25 17:45:05 +0300

    feat: add mapper layer and use it on all layers

--- src/main/java/mate/academy/onlinebookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.onlinebookstore.repository.impl;
 
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore.exception.DataProcessingException;
 import mate.academy.onlinebookstore.model.Book;
@@ -43,6 +44,14 @@ public class BookRepositoryImpl implements BookRepository {
             return session.createQuery("FROM Book", Book.class).getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't find books", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Book book = session.find(Book.class, id);
+            return Optional.ofNullable(book);
         }
     }
 }
