8895e9b2c0acd317c515a565dacab90c525e4bc2
Denya Basarab <denya2007basarab@gmail.com>
2026-08-04 15:23:11 +0300

    feat: add dto validation

--- src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,12 +1,26 @@
 package mate.academy.onlinebookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
+import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.URL;
 
 public record CreateBookRequestDto(
+        @NotBlank
+        @Size(max = 255)
         String title,
+        @NotBlank
+        @Size(max = 255)
         String author,
+        @NotBlank
+        @ISBN
         String isbn,
+        @Positive
         BigDecimal price,
+        @Size(max = 255)
         String description,
+        @URL
         String coverImage) {
 }
