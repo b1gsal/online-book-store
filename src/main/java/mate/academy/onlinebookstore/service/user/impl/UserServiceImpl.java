39202e65186a661c73803c2eebf43ba8b669a0a5
Denya Basarab <denya2007basarab@gmail.com>
2026-08-15 18:05:53 +0300

    feat: add config for security

--- src/main/java/mate/academy/onlinebookstore/service/user/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/onlinebookstore/service/user/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package mate.academy.onlinebookstore.service.user.impl;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import mate.academy.onlinebookstore.dto.user.UserResponseDto;
 import mate.academy.onlinebookstore.exception.RegistrationException;
 import mate.academy.onlinebookstore.mapper.UserMapper;
+import mate.academy.onlinebookstore.model.Role;
+import mate.academy.onlinebookstore.model.RoleName;
 import mate.academy.onlinebookstore.model.User;
+import mate.academy.onlinebookstore.repository.role.RoleRepository;
 import mate.academy.onlinebookstore.repository.user.UserRepository;
 import mate.academy.onlinebookstore.service.user.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -15,6 +20,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder encoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
@@ -23,6 +30,11 @@ public class UserServiceImpl implements UserService {
                     + requestDto.email());
         }
         User model = userMapper.toModel(requestDto);
+        model.setPassword(encoder.encode(requestDto.password()));
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RegistrationException("Can't find role "
+                        + RoleName.ROLE_USER));
+        model.setRoles(Set.of(userRole));
         User savedUser = userRepository.save(model);
         return userMapper.toDto(savedUser);
     }
